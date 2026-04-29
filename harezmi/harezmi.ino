870edf604498244923d013e9ba137379ae6db35c
Batuhan <158720104+MrBATUSS@users.noreply.github.com>
2026-04-30 02:35:59 +0300

    Güncelleme
    Boot_flags integrated

--- harezmi/harezmi.ino
+++ harezmi/harezmi.ino
@@ -13,12 +13,16 @@ int mq136_pin = A2;  //ANALOG 2 MQ-136
 int mq136_value;
 int mq135_value;
 int mq4_value;
+
+//FLAGS
+bool first_boot = true;
  
 void setup() {
   Serial.begin(9600);                            //BILGISAYAR ILE ILETISIMI **9600 HIZINDA** BASLATIYORUZ BURDAN GELEN VERILERI OKUYCAZ
   pinMode(mq4_pin, INPUT);
   pinMode(mq135_pin, INPUT);
   pinMode(mq136_pin, INPUT);
+  pinMode(2,OUTPUT);
 
   lcd.init();
   lcd.backlight();
@@ -38,7 +42,6 @@ void mq135() {
 void mq4() {
   mq4_value = analogRead(mq4_pin);               //MQ-4 IN DONDURDUGU DEGERI DEGISKENE VERIYORUZ
   Serial.println("MQ-4: " + String(mq4_value));  //VERIYI EKRANA YAZDIRIYORUZ
-  //FALAN FILAN - REDD
 }
 
 void screen() {
@@ -58,6 +61,21 @@ void screen() {
 }
 
 void loop() {
+
+  if(first_boot == true) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("ILK ACILISTA 24S");
+    lcd.setCursor(0,1);
+    lcd.print("ISINMA BEKLENIYOR");
+    
+    delay(5000); 
+    
+    first_boot = false; // Bir kez çalıştıktan sonra false yap ki döngüye girmesin
+  }
+
+  digitalWrite(2,HIGH);
+
   delay(1000);
   //mq4();
   mq135();
