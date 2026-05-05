77f349c667d11ca859535757f1c5f37e55229b5c
Batuhan <158720104+MrBATUSS@users.noreply.github.com>
2026-05-06 02:04:08 +0300

    Update main.cpp
    - Added Kalibration Mod
    - Added DEV_MOD and Normal Mod
    - Optimized Screen
    - Discarted MQ-4 Co2 Sensor
    - Project Moved to PlatformIO base

--- src/main.cpp
+++ src/main.cpp
@@ -4,80 +4,153 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//SENSORLER
-int mq4_pin = A0;    //ANALOG 0 MQ-4
-int mq135_pin = A1;  //ANALOG 1 MQ-135
-int mq136_pin = A2;  //ANALOG 2 MQ-136
+// SENSORLER
+int mq135_pin = A0; // ANALOG 1 MQ-135
+int mq136_pin = A1; // ANALOG 2 MQ-136
+int buzzer_pin = 3;
 
-//VERILER
-int mq136_value;
-int mq135_value;
-int mq4_value;
+// VERILER
+int mq136_value, mq136_maxValue = 100;
+int mq135_value, mq135_maxValue = 100;
 
-//FLAGS
-bool first_boot = true;
+float mq135_base = 0; // Ortamın normal MQ135 değeri
+float mq136_base = 0; // Ortamın normal MQ136 değeri
 
-void setup() {
+int heat_time = 180; // 3 DAKIKA 180 SANIYE
+
+// FLAGS
+bool DEV_MOD = true;
+
+void setup()
+{
   Serial.begin(9600);
-  pinMode(mq4_pin, INPUT);
+
   pinMode(mq135_pin, INPUT);
   pinMode(mq136_pin, INPUT);
-  pinMode(2,OUTPUT);
+  pinMode(buzzer_pin, OUTPUT);
 
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
-}
 
-void mq136() {
-  mq136_value = analogRead(mq136_pin);                  
-  Serial.println("--MQ136--: " + String(mq136_value));  
-}
+  digitalWrite(buzzer_pin, HIGH);
+  delay(500);
+  digitalWrite(buzzer_pin, LOW);
 
-void mq135() {
-  mq135_value = analogRead(mq135_pin);              
-  Serial.println("MQ-135: " + String(mq135_value));  
-}
+  if (!DEV_MOD)
+  {
+    for (int i = heat_time; i > 0; i--)
+    {
+      int min = i / 60;
+      int sec = i % 60;
 
-void mq4() {
-  mq4_value = analogRead(mq4_pin);        
-  Serial.println("MQ-4: " + String(mq4_value));
-}
+      lcd.clear();
 
-void screen() {
-  lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Heating Up...");
 
-  //MQ_136
-  lcd.setCursor(0, 0);
-  lcd.print("MQ_136: ");
-  lcd.setCursor(7,0);
-  lcd.print(mq136_value);
+      lcd.setCursor(0, 1);
+      lcd.print("Pls Wait: ");
 
-  //MQ_135
-  lcd.setCursor(0, 1);
-  lcd.print("MQ_135:");
-  lcd.setCursor(7,1);
-  lcd.print(mq135_value);
-}
+      if (min < 10)
+        lcd.print("0");
+      lcd.print(min);
 
-void loop() {
+      lcd.print(":");
 
-  if(first_boot == true) {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("ILK ACILISTA 24S");
-    lcd.setCursor(0,1);
-    lcd.print("ISINMA BEKLENIYOR");
+      if (sec < 10)
+        lcd.print("0");
+      lcd.print(sec);
 
-    delay(5000);
-
-    first_boot = false; // Bir kez çalıştıktan sonra false yap ki döngüye girmesin
+      delay(1000);
+    }
   }
 
-  digitalWrite(2,HIGH);
+  lcd.clear();
+  lcd.print("Ready to Test!");
+  delay(2000);
+  lcd.clear();
+}
 
+void alarm()
+{
+  for (int i = 0; i < 6; i++)
+  {
+    digitalWrite(buzzer_pin, HIGH);
+    delay(150);
+    digitalWrite(buzzer_pin, LOW);
+    delay(150);
+  }
+}
+
+void calibrating()
+{
+  lcd.clear();
+  lcd.print("Calibrating...");
+
+  float total135 = 0;
+  float total136 = 0;
+  int sample_count = 50;
+
+  for (int i = 0; i < sample_count; i++)
+  {
+    total135 += analogRead(mq135_pin); // Değerleri üst üste ekle
+    total136 += analogRead(mq136_pin);
+
+    // İlerleme çubuğu gibi bir görsel (isteğe bağlı)
+    lcd.setCursor(0, 1);
+    lcd.print("Progress: %");
+    lcd.print((i * 100) / sample_count);
+
+    delay(100); // Okumalar arasında kısa bir duraksama
+  }
+
+  mq135_base = total135 / sample_count;
+  mq136_base = total136 / sample_count;
+
+  mq135_maxValue = mq135_base * 1.30;
+  mq136_maxValue = mq136_base * 1.30;
+
+  lcd.clear();
+  lcd.print("Done!");
   delay(1000);
-  mq135();
-  mq136();
-  screen();
+}
+
+void loop()
+{
+  mq136_value = analogRead(mq136_pin);
+  mq135_value = analogRead(mq135_pin);
+
+  if (!DEV_MOD)
+  {
+    lcd.setCursor(0, 1);
+
+    if (mq135_value > (mq135_base * 1.45) || mq136_value > (mq136_base * 1.45))
+    {
+      lcd.print("DURUM: SAKINCALI");
+      alarm();
+    }
+    else if (mq135_value >= mq135_maxValue || mq136_value >= mq136_maxValue)
+    {
+      lcd.print("DURUM: RİSKLİ");
+      digitalWrite(buzzer_pin, HIGH);
+      delay(100);
+      digitalWrite(buzzer_pin, LOW);
+    }
+    else
+    {
+      lcd.print("Durum: Taze");
+    }
+  }
+  else
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("V135:");
+    lcd.print(mq135_value);
+    lcd.print("  ");
+    lcd.setCursor(8, 0);
+    lcd.print("V136:");
+    lcd.print(mq136_value);
+    lcd.print("  ");
+  }
+  delay(1000);
 }
